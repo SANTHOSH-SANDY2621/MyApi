e65cbe33411d03a6d84d48e152ec31d340103db4
Santhosh S <santhosh136sandy@gmail.com>
2025-05-13 13:34:22 +0530

    initial

---
+++ MyApi/Controllers/MasterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasterController : ControllerBase
+    {
+    }
+}
